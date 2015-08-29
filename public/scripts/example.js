437f95f2f073efb32a9d88ed4abf40ffdaca9a62
andygout <andygout@hotmail.co.uk>
2015-08-29 22:25:04 +0100

    Add: Component properties to CommentList

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -27,7 +27,8 @@ var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment author="Andy Gout">This is one comment</Comment>
+        <Comment author="Xin-ying Kowa">This is *another* comment</Comment>
       </div>
     );
   }
